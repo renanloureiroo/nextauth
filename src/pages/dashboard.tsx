5ad6988d0625258b106fc7b80514d350a763e157
Renan Pereira Loureiro <renanpl47@gmail.com>
2022-03-22 10:59:21 -0300

    feat: validando permissions

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,9 @@
 import { destroyCookie } from "nookies"
 import { useContext, useEffect } from "react"
+import { Can } from "../components/Can"
 import { AuthContext } from "../contexts/AuthContext"
 import { AuthTokenError } from "../errors/AuthTokenError"
+import { useCan } from "../hooks/useCan"
 import { setupAPIClient } from "../services/api"
 import { api } from "../services/apiClient"
 import { withSSRAuth } from "../utils/withSSRAuth"
@@ -15,7 +17,14 @@ const Dashboard = () => {
       .then((response) => console.log(response))
       .catch((err) => console.log(err))
   }, [])
-  return <h1>Dashboard: {user?.email} </h1>
+  return (
+    <>
+      <h1>Dashboard: {user?.email} </h1>
+      <Can permissions={["metrics.list"]}>
+        <div>Métricas</div>
+      </Can>
+    </>
+  )
 }
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
